b1faf55f23b5d1c6ae402a948e57ad542d980b9e
genafalconi <genaro.f@hotmail.com>
2023-08-25 20:05:40 -0300

    add highlight subprod

--- src/subproducts/subproducts.controller.ts
+++ src/subproducts/subproducts.controller.ts
@@ -2,6 +2,7 @@ import {
   Body,
   Controller,
   Delete,
+  Get,
   Inject,
   Param,
   Post,
@@ -11,22 +12,28 @@ import { LockDto } from 'src/dto/lock.dto';
 import { FirebaseAuthGuard } from 'src/firebase/firebase.auth.guard';
 import { SubproductsService } from './subproducts.service';
 import { Lock } from 'src/schemas/lock.schema';
+import { Subproduct } from 'src/schemas/subprod.schema';
 
-@UseGuards(FirebaseAuthGuard)
 @Controller('subproducts')
 export class SubproductsController {
   constructor(
     @Inject(SubproductsService)
     private readonly subproductsService: SubproductsService,
-  ) {}
-
+  ) { }
+  
+  @UseGuards(FirebaseAuthGuard)
   @Post('/lock')
   async lockSubprods(@Body() dataLockSubprods: LockDto): Promise<Lock[]> {
     return await this.subproductsService.lockSubprods(dataLockSubprods);
   }
-
+  @UseGuards(FirebaseAuthGuard)
   @Delete('/lock/:idUser')
   async deleteLockSubprods(@Param('idUser') user: string): Promise<Lock[]> {
     return await this.subproductsService.removeLockUser(user);
+  }
+
+  @Get('/highlight')
+  async getHighlightedSubprods(): Promise<Subproduct[]> {
+    return await this.subproductsService.getHighlightedSubprods()
   }
 }
